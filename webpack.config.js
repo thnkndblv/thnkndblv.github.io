8b88ed2fc9a82196a859a42550c23f97ae7727ba
Manuel Morales <manuel.cpp@gmail.com>
2018-11-21 16:22:50 -0600

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/dist');
+const webpack = require('webpack')
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.jsx$/,
+      include: SRC_DIR,
+      loader: 'babel-loader',
+      query: {
+        presets: ["@babel/preset-env","@babel/preset-react"]
+      }
+    }]
+  }
+};
