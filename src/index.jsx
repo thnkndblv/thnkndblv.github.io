49d05164e3142d1207800d1b4c513d85d90be9fe
Manuel Morales <manuel.cpp@gmail.com>
2018-12-10 20:18:30 -0600

    Include styles into the entry point

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
+import './stylesheet/main.scss';
+
 ReactDOM.render(<App />, document.getElementById('app'));
