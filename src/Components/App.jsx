db3a225fc0241cf34126add21a54b7d5228ba322
Manuel Morales <manuel.cpp@gmail.com>
2018-12-10 22:05:28 -0600

    Add contact methods section

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ResumeHeader from './ResumeHeader';
 import ResumeContent from './ResumeContent';
-import { name, position } from '../data/resume.json';
+import { name, position, contact } from '../data/resume.json';
 
 const App = () => (
   <div>
     <ResumeHeader name={name} position={position} />
-    <ResumeContent />
+    <ResumeContent contact={contact} />
   </div>
 );
 
--- src/Components/ResumeContent.jsx
+++ src/Components/ResumeContent.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import ContactMethods from './Resume/ContactMethods/ContactMethods';
 
-const ResumeContent = ({}) => (
+const ResumeContent = ({ contact }) => (
   <div className="row resume--content">
-    <div className="col-4"></div>
+    <div className="col-4">
+      <ContactMethods
+        phone={contact.phone}
+        email={contact.email}
+        address={contact.address}
+        linkedin={contact.linkedin} />
+
+    </div>
     <div className="col-8 resume--content__right"></div>
   </div>
 );
